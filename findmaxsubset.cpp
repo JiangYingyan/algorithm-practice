82952e866481c7f893e5800e34390e0d054bfcb1
JiangYingyan <790568256@qq.com>
2018-05-29 13:56:48 +0800

    动态规划寻找最大子集
    如果集合内全是非正数，则输出空集

--- findmaxsubset.cpp
+++ findmaxsubset.cpp
@@ -11,8 +11,8 @@ int main(){
     int LastIndex = 0;
     int Max = 0;
     int Maxtemp = 0;
-    int Index1 = 0;
-    int Index2 = 0;
+    int Index1 = -1;
+    int Index2 = -1;
     int MarixSize;
     cout << "input marix size(no more than 10000):";
     cin >> MarixSize;
@@ -23,13 +23,15 @@ int main(){
     }
     while(LastIndex < MarixSize){
         if(LastIndex == FirstIndex){
-            if(A[LastIndex] < 0 ){
-                 FirstIndex = LastIndex = LastIndex++;
+            if(A[LastIndex] <= 0 ){
+                 LastIndex++;
+                 FirstIndex = LastIndex;
             }
             else{
+
                 Maxtemp = A[FirstIndex];
                 Max = Compare(Max, Maxtemp);
-                if(Max = Maxtemp){
+                if(Max == Maxtemp){
                     Index1 = FirstIndex;
                     Index2 = LastIndex;
                 }
@@ -37,24 +39,36 @@ int main(){
             }
         }
         if(LastIndex > FirstIndex){
-            if(Maxtemp + A[LastIndex] < 0){
-                FirstIndex = LastIndex = LastIndex++;
+            if(Maxtemp + A[LastIndex] <= 0){
+                LastIndex++;
+                FirstIndex = LastIndex;
                 Maxtemp = 0;
             }
             if(Maxtemp + A[LastIndex] > 0){
                 Maxtemp = Maxtemp + A[LastIndex];
                  Max = Compare(Max, Maxtemp);
-                if(Max = Maxtemp){
+                if(Max == Maxtemp){
                     Index1 = FirstIndex;
                     Index2 = LastIndex;
                 }
                 LastIndex++;
-
             }
-
         }
     }
-    cout << Index1 <<"/n" << Index2;
+    if(Index2 >= 0)
+        cout << "First index is:" << Index1+1 <<"\n" <<"Second index is: " << Index2 + 1 <<"\n";
+    cout <<"The max subset is :";
+    int sum = 0;
+    if(Index2 == -1){
+        cout << "empty set";
+    }
+    else{
+        for(int i = Index1;i <= Index2;i++){
+            sum += A[i];
+            cout << A[i] <<" ";
+        }
+    }
+    cout << "\n"<<"The sum is:" << sum;
     return 0;
 }
 
